e3b14cc34c7a7663d5fdf74212400be5ab315cd7
Shreya Oswal <oswalshreya1998@gmail.com>
2022-11-01 19:07:32 -0400

    Updated Map function

--- frontend/src/Body/components/Map.js
+++ frontend/src/Body/components/Map.js
@@ -1,90 +1,47 @@
- 
-
-import { useMemo } from "react";
-import { GoogleMap, useLoadScript, MarkerF,KmlLayer } from "@react-google-maps/api";
+import React from 'react'
+import { GoogleMap, useJsApiLoader,KmlLayer } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '800px',
-  height: '800px'
+  width: '400px',
+  height: '400px'
 };
-export default function RenderMap() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: ''
-  });
 
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Map />;
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function RenderMap() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'AIzaSyD5ernPUMCOp0QHET9gPW5XPyHVKpHVn5E'
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+
+  <KmlLayer url='https://getcapstonebucket.s3.us-east-2.amazonaws.com/westcampus.kml'/>
+       
+      </GoogleMap>
+  ) : <></>
 }
 
-function Map() {
-  //return <div>Map</div>
-   const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
-  // return (
-  //   <GoogleMap zoom={10} center={center} mapContainerStyle={containerStyle}>
-  //     <KmlLayer url='https://getcapstonebucket.s3.us-east-2.amazonaws.com/westcampus.kml'/>
-  //   </GoogleMap>
-  // );
-
-
-  return (
-    <GoogleMap zoom={10} center={center} mapContainerStyle={containerStyle}>
-      <MarkerF position={center} />
-    </GoogleMap>
-  );
-
-  
-}
-
-//import React, { Component } from 'react';
-// import { withScriptjs, withGoogleMap, GoogleMap, Marker, KmlLayer } from "react-google-maps"
-
-// const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-//   <GoogleMap
-//     defaultZoom={8}
-//     defaultCenter={{ lat: 50.5010789, lng: 4.4764595 }}
-//   >
-//     <KmlLayer 
-//         url='https://getcapstonebucket.s3.us-east-2.amazonaws.com/westcampus.kml'
-//         options={{ preserveViewport : false}}
-//     />
-//     {props.isMarkerShown && <Marker position={{ lat: 50.5010789, lng: 4.4764595 }} />}
-//   </GoogleMap>
-// ))
-
-// export default class Map extends Component {
-//   render(){
-//       return( 
-//         <div>
-//         <p>Hello</p>
-
-//           <MyMapComponent
-//            containerElement={
-//             <div style={{height: `1000px`, width: `1000px`}} />
-//           }
-    
-//           mapElement={
-//             <div style={{height: `1000px`, width: `1000px`}} />
-//           }
-    
-//             isMarkerShown
-//             googleMapURL="https://maps.googleapis.com/maps/api/js?key=&v=3.exp&libraries=geometry,drawing,places"
-//             loadingElement={<div style={{ height: `100%` }} />}
-//             //containerElement={<div style={{ height: `100%` }} />}
-//             //mapElement={<div style={{ height: `100%` }} />}
-//           />
-//           </div>
-          
-//       )
-//   }
-// }
-
-// // const Map = () => {
-// //   return (
-// //     <div style={{padding:"200px"}}>
-// //       map-place-holder
-// //     </div>
-// //   )
-// // }
-
-// // export default Map
-// //export default MyMapComponent
+export default React.memo(RenderMap)
