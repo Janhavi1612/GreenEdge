3e4e5e00a840bbf581f6d6a4a5eb2374e7d507bd
xiaobing S <xiaobings@xiaobingdembp.wifi.local.cmu.edu>
2022-11-06 15:46:19 -0500

    Proof of Concept of Export Function

--- frontend/src/Body/components/Export.js
+++ frontend/src/Body/components/Export.js
@@ -1,11 +1,32 @@
 import React, { useState } from 'react'
 import { DownloadOutlined } from '@ant-design/icons';
 import { Button} from 'antd';
+import { saveAs } from "file-saver";
+import { Packer } from "docx";
+import { experiences, education, skills, achievements } from "./data/cv-data.ts";
+import { DocumentCreator } from "./data/cv-generator.ts";
 const Export = () => {
   const [size, setSize] = useState('medium');
+  const generateWordDocument = () =>{
+    const documentCreator = new DocumentCreator();
+    const doc = documentCreator.create([
+      experiences,
+      education,
+      skills,
+      achievements
+    ]);
+  
+    Packer.toBlob(doc).then(blob => {
+      console.log(blob);
+      saveAs(blob, "example.docx");
+      console.log("Document created successfully");
+    });
+  
+  }
+  
   return (
-    <Button type="primary" shape="round" icon={<DownloadOutlined />} size={size}>
-        Export
+    <Button onClick={generateWordDocument} type="primary" shape="round" icon={<DownloadOutlined />} size={size}>
+        Export docx file
       </Button>
   )
 }
