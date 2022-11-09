7d8971c7151f2f5e1929f527fcd303025c8b0842
xiaobing S <xiaobings@xiaobingdembp.wifi.local.cmu.edu>
2022-11-09 18:49:22 -0500

    toy export component

--- frontend/src/Body/components/Export.js
+++ frontend/src/Body/components/Export.js
@@ -4,27 +4,27 @@ import { Button} from 'antd';
 import { saveAs } from "file-saver";
 import { Packer } from "docx";
 import { experiences, education, skills, achievements } from "./data/cv-data.ts";
-import { DocumentCreator } from "./data/report-generator.ts";
-// import { DocumentCreator } from "./data/cv-generator.ts";
+import { DocumentCreator as DocumentGenerator } from "./data/report-generator.ts";
+import { DocumentCreator } from "./data/cv-generator.ts";
 const Export = () => {
   const [size, setSize] = useState('medium');
-  // const generateWordDocument = () =>{
-  //   const documentCreator = new DocumentCreator();
-  //   const doc = documentCreator.create([
-  //     experiences,
-  //     education,
-  //     skills,
-  //     achievements
-  //   ]);
-  
-  //   Packer.toBlob(doc).then(blob => {
-  //     console.log(blob);
-  //     saveAs(blob, "example.docx");
-  //     console.log("Document created successfully");
-  //   });
-  // }
-  const generateReportDocument = () =>{
+  const generateWordDocument = () =>{
     const documentCreator = new DocumentCreator();
+    const doc = documentCreator.create([
+      experiences,
+      education,
+      skills,
+      achievements
+    ]);
+  
+    Packer.toBlob(doc).then(blob => {
+      console.log(blob);
+      saveAs(blob, "example.docx");
+      console.log("Document created successfully");
+    });
+  }
+  const generateReportDocument = () =>{
+    const documentCreator = new DocumentGenerator();
     const doc = documentCreator.create();
   
     Packer.toBlob(doc).then(blob => {
@@ -40,7 +40,7 @@ const Export = () => {
         Export docx file
         </Button> */}
         <Button onClick={generateReportDocument} type="primary" shape="round" icon={<DownloadOutlined />} size={size}>
-        Export docx file
+        Export report
         </Button>
     </div>
 
